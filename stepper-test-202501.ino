7a1b5add55f0f71b4e7243b60a431e7ee3a754a5
Nick Matantsev <nick.matantsev@gmail.com>
2025-01-05 14:19:10 -0500

    Initial commit

---
+++ stepper-test-202501.ino
@@ -0,0 +1,18 @@
+#include <Stepper.h>
+
+// actually ~2036.8? per https://www.youtube.com/watch?v=B86nqDRskVU
+const int stepsPerRevolution = 2038;
+
+// IN1-IN4 = pin 8-11
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+
+void setup() {
+  // speed at 2 RPM (3+ starts being weak and stalling out)
+  myStepper.setSpeed(2);
+}
+
+void loop() {
+  // 90deg turn and delay
+  myStepper.step(stepsPerRevolution / 4);
+  delay(500);
+}
