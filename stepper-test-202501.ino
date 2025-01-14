619fdcebc9a851c8b41aa6c44844b1c77d5cfedd
Nick Matantsev <nick.matantsev@gmail.com>
2025-01-14 16:57:02 -0500

    Add direct joystick control

--- stepper-test-202501.ino
+++ stepper-test-202501.ino
@@ -6,13 +6,42 @@ const int stepsPerRevolution = 2038;
 // IN1-IN4 = pin 8-11
 Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
 
+// const int pinLeft = 2;
+// const int pinRight = 3;
+const int analogInPin = A0;
+
+int dirState = 0;
+
 void setup() {
+  // input pins
+  // pinMode(pinLeft, INPUT_PULLUP);
+  // pinMode(pinRight, INPUT_PULLUP);
+  pinMode(analogInPin, INPUT);
+
   // speed at 4 RPM (5+ starts being weak and stalling out)
-  myStepper.setSpeed(4);
+  // myStepper.setSpeed(4);
 }
 
 void loop() {
-  // 90deg turn and delay
-  myStepper.step(-stepsPerRevolution / 4);
-  delay(500);
+  if (dirState < -1) {
+    myStepper.step(-1);
+  } else if (dirState > 1) {
+    myStepper.step(1);
+  }
+
+  // check for button press
+  if (analogRead(analogInPin) < 200) {
+    dirState = max(-4, dirState - 1);
+  } else if (analogRead(analogInPin) > 823) {
+    dirState = min(4, dirState + 1);
+  } else {
+    // return to center
+    if (dirState > 0) {
+      dirState--;
+    } else if (dirState < 0) {
+      dirState++;
+    }
+  }
+
+  delay(5); // 4rpm = ~7.5ms per step
 }
